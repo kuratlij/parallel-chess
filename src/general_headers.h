499b79eab4fd4558bee75c1b7f1c56a8863fb932
JR <rosenthj@student.ethz.ch>
2016-11-01 17:39:00 +0100

    Added general header file which can be included by all standard files and should give access to useful functions such as debug and types.

---
+++ src/general_headers.h
@@ -0,0 +1,18 @@
+/*
+ * general_headers.h
+ *
+ * The point of this header is to group headers of generally useful utility
+ * files together into one in order to clean up code.
+ *
+ *  Created on: Nov 1, 2016
+ *      Author: jonathan
+ */
+
+#ifndef GENERAL_HEADERS_H_
+#define GENERAL_HEADERS_H_
+
+#include "general/types.h"
+#include "general/debug.h"
+
+
+#endif /* GENERAL_HEADERS_H_ */
