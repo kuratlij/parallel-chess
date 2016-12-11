9062fbf16df04d0430b5b7531e625ab9e5601bb4
maurerjo <maurerjo@ethz.ch>
2016-12-11 15:34:24 +0100

    spinlock implementation  based on atomic_flag  lock(): waits until thread gets lock, then preseeds  unlock(): frees lock

---
+++ src/general/spin_lock.cc
@@ -0,0 +1,15 @@
+//
+// Created by jonathan on 11.12.16.
+//
+#include <atomic>
+class spin_lock {
+    std::atomic_flag locked = ATOMIC_FLAG_INIT;
+
+    void lock(){
+        while(locked.test_and_set( std::memory_order_acquire ) )
+            ;
+    }
+    void unlock(){
+        locked.clear(std::memory_order_release);
+    }
+};
