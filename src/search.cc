7d33f03dc7f68ef8af146c0d69c2967b07f4731e
maurerjo <maurerjo@ethz.ch>
2016-12-12 11:18:42 +0100

    added parallel alpha-beta search, not finished yet

--- src/search.cc
+++ src/search.cc
@@ -54,39 +54,96 @@ long Perft(Board board, Depth depth) {
   return perft_sum;
 }
 
-Score AlphaBeta(Board board, Score alpha, Score beta, Depth depth) {
-  if (depth == 0) {
-    return evaluation::ScoreBoard(board);
-  }
-  std::vector<Move> moves = board.GetMoves();
-  if (moves.size() == 0) {
-    if (board.InCheck()) {
-      return kMinScore;
+    Score AlphaBeta(Board board, Score alpha, Score beta, Depth depth) {
+        if (depth == 0) {
+            return evaluation::ScoreBoard(board);
+        }
+        std::vector<Move> moves = board.GetMoves();
+        if (moves.size() == 0) {
+            if (board.InCheck()) {
+                return kMinScore;
+            }
+            return 0;
+        }
+        table::Entry entry = table::GetEntry(board.get_hash());
+        if (table::ValidateHash(entry,board.get_hash())) {
+            std::sort(moves.begin(), moves.end(), Sorter(entry.best_move));
+        }
+        Move best_local_move = moves[0];
+        for (Move move : moves) {
+            board.Make(move);
+            Score score = -AlphaBeta(board, -beta, -alpha, depth-1);
+            board.UnMake();
+            if (score >= beta) {
+                best_move = move;
+                table::SaveEntry(board.get_hash(), move, score);
+                return score;
+            }
+            if (score > alpha) {
+                alpha = score;
+                best_local_move = move;
+            }
+        }
+        best_move = best_local_move;
+        return alpha;
     }
-    return 0;
-  }
-  table::Entry entry = table::GetEntry(board.get_hash());
-  if (table::ValidateHash(entry,board.get_hash())) {
-    std::sort(moves.begin(), moves.end(), Sorter(entry.best_move));
-  }
-  Move best_local_move = moves[0];
-  for (Move move : moves) {
-    board.Make(move);
-    Score score = -AlphaBeta(board, -beta, -alpha, depth-1);
-    board.UnMake();
-    if (score >= beta) {
-      best_move = move;
-      table::SaveEntry(board.get_hash(), move, score);
-      return score;
+
+    Score ParallelAlphaBeta(Board board, Score alpha, Score beta, Depth depth) {
+        if (depth == 0) {
+            return evaluation::ScoreBoard(board);
+        }
+        std::vector<Move> moves = board.GetMoves();
+        if (moves.size() == 0) {
+            if (board.InCheck()) {
+                return kMinScore;
+            }
+            return 0;
+        }
+        table::Entry entry = table::GetEntry(board.get_hash());
+        if (table::ValidateHash(entry,board.get_hash())) {
+            std::sort(moves.begin(), moves.end(), Sorter(entry.best_move));
+        }
+        std::vector<Move> best_local_move = std::vector<Move>(settings::num_threads);
+        std::vector<Board> parallelBoard = std::vector<Board>(settings::num_threads);
+        std::vector<Score> parallelAlpha = std::vector<Score>(settings::num_threads);
+        for(int i = 0; i<settings::num_threads;i++){
+            best_local_move[i] = moves[0];
+            parallelBoard[i] = board;//Todo make copy, don't reference
+            parallelAlpha[i] = alpha;//Todo make copy, don't reference
+        }
+        omp_set_num_threads(settings::num_threads);
+        bool go = true;
+        Score end_score;
+#pragma omp parallel for
+        for ( int i = 0;(i<moves.size());i++) {
+            if(go){
+                Move move = moves[i];
+                parallelBoard[omp_get_thread_num()].Make(move);
+                Score score = -AlphaBeta(board, -beta, -parallelAlpha[omp_get_thread_num()], depth - 1);
+                parallelBoard[omp_get_thread_num()].UnMake();
+                if (score >= beta) {
+                    best_move = move;
+                    table::SaveEntry(board.get_hash(), move, score);
+                    go=false;
+                    #pragma omp flush(go)
+                    end_score = score;
+                }
+                if (score > parallelAlpha[omp_get_thread_num()]) {
+                    parallelAlpha[omp_get_thread_num()] = score;
+                    best_local_move[omp_get_thread_num()] = move;
+                }
+            }
+        }
+        if(!go){
+            return end_score;
+        }
+        best_move = best_local_move[0];
+        return alpha;
     }
-    if (score > alpha) {
-      alpha = score;
-      best_local_move = move;
-    }
-  }
-  best_move = best_local_move;
-  return alpha;
-}
+
+
+
+
 
 Move DepthSearch(Board board, Depth depth) {
   Move bestmoveSeq = SequentialSearch(board, depth);
