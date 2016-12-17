9434dde6b9f5ed4a98804e15265220b1f203e6e6
kuratlij <kuratlij@student.ethz.ch>
2016-12-18 00:13:36 +0100

    Testing, params can be changed in main.cc

--- fens/load_fen.py
+++ fens/load_fen.py
@@ -1,16 +1,22 @@
 import selenium
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 # Load page
 
 browser = webdriver.Chrome()
 browser.get("http://bernd.bplaced.net/fengenerator/fengenerator.html")
 
-int pieces = 16
+pieces = 32
+amount = 100
 
 f = open("random_fens_" + str(pieces), "w")
 
-for i in xrange(100):
+for i in xrange(amount):
+    # Find dropdown menu for no of pieces
+    dropdown = Select(browser.find_element_by_id("sfig"))
+    dropdown.select_by_visible_text(str(pieces))
+    
     button = browser.find_element_by_css_selector("input[onclick*='makefen()']")
     button.click()
 
