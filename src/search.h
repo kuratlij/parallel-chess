18826b57dc2cbe9b0fffeef3c79ae83a38129e0f
JR <rosenthj@student.ethz.ch>
2016-12-18 12:00:20 +0100

    Adjusted depth pattern to accept alpha and beta as perameters

--- src/search.h
+++ src/search.h
@@ -30,7 +30,7 @@ Move SequentialSearch(Board board, Depth depth, Time end_time);
 Move TestSequentialSearch(Board board, Depth depth, std::string file_path);
 Move ParallelSearch(Board board, Depth depth, Time end_time);
     Move TestParallelSearch(Board board, Depth depth, std::string file_path);
-bool depthPattern(Depth depth);
+bool depthPattern(Depth depth, Score alpha, Score beta);
 bool finished(Time end_time);
 Score AlphaBeta(Board board, Score alpha, Score beta, Depth depth, Time end_time);
 Score ParallelAlphaBeta(Board board, Score alpha, Score beta, Depth depth, Time end_time);
