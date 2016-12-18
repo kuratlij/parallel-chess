761099438c366e3fa85d91e61664399632fc3e5b
maurerjo <maurerjo@ethz.ch>
2016-12-18 13:18:38 +0100

    implemented YBWC

--- src/search.cc
+++ src/search.cc
@@ -223,6 +223,119 @@ Score ParallelAlphaBeta(Board board, Score alpha, Score beta, Depth depth, Time 
   return alpha;
 }
 
+Score ParallelAlphaBetaYBWC(Board board, Score alpha, Score beta, Depth depth, Time end_time) {
+  debug::SearchDebug("ps t"+std::to_string(omp_get_thread_num())+" "+std::to_string(alpha)+","+std::to_string(beta), depth);
+  if (depth == 0) {
+    return evaluation::ScoreBoard(board);
+  }
+  std::vector<Move> moves = board.GetMoves();
+  if (moves.size() == 0) {
+    if (board.InCheck()) {
+      return kMinScore;
+    }
+    return 0;
+  }
+  table::Entry entry = table::GetEntry(board.get_hash());
+  if (table::ValidateHash(entry,board.get_hash())) {
+    std::sort(moves.begin(), moves.end(), Sorter(entry.best_move));
+  }
+  bool go = true;
+  omp_set_num_threads(settings::num_threads);
+  omp_set_nested(1);
+  Move first_move = moves[0];
+  Score first_alpha;
+  board.Make(first_move);
+  if(parallel_pattern(depth, alpha, beta)){
+    first_alpha = -ParallelAlphaBeta(board,-beta, -alpha, depth-1, end_time);
+  } else {
+    first_alpha = -AlphaBeta(board,-beta, -alpha, depth-1, end_time);
+  }
+  board.UnMake();
+  if(first_alpha>=beta){
+    return beta;
+  }
+  if(alpha<first_alpha){
+    alpha=first_alpha;
+  }
+#pragma omp parallel
+  {
+    std::vector<Move> private_moves = moves;
+    Score privateAlpha = alpha;
+    Board privateBoard = board.copy();
+    Move private_best_local_move = private_moves[0];
+    for (int i = omp_get_thread_num()+1; i < private_moves.size(); i+=settings::num_threads) {
+      if (!go || finished(end_time)){
+        break;//already found better score then beta, skip rest of computation
+      }
+      Move move = private_moves[i];
+      privateBoard.Make(move);
+      debug::SearchDebug(">>"+parse::MoveToString(move)+" pt"+std::to_string(omp_get_thread_num()), depth);
+      if (settings::alphaPropagation && (alpha > privateAlpha)) {
+        privateAlpha = alpha;
+      }
+      Score score;
+      if (is_null_window(privateAlpha, beta)) {
+        debug::SearchDebug("["+std::to_string(privateAlpha)+","+std::to_string(beta)+"] pt"+std::to_string(omp_get_thread_num()), depth);
+        if (parallel_pattern(depth, privateAlpha, beta)) {
+          score = -ParallelAlphaBeta(privateBoard, -beta, -privateAlpha, depth - 1, end_time);
+        }
+        else {
+          score = -AlphaBeta(privateBoard, -beta, -privateAlpha, depth - 1 , end_time);
+        }
+      }
+      else {
+        debug::SearchDebug("["+std::to_string(privateAlpha)+","+std::to_string(privateAlpha+1)+"] pt"+std::to_string(omp_get_thread_num()), depth);
+        if(parallel_pattern(depth, privateAlpha, beta)){
+          score = -ParallelAlphaBeta(privateBoard, -(privateAlpha+1), -privateAlpha, depth - 1, end_time);
+          if (score >= (privateAlpha+1)) {
+            debug::SearchDebug("["+std::to_string(privateAlpha)+","+std::to_string(beta)+"] pt"+std::to_string(omp_get_thread_num()), depth);
+            score = -ParallelAlphaBeta(privateBoard, -beta, -privateAlpha, depth - 1, end_time);
+          }
+        }
+        else {
+          score = -AlphaBeta(privateBoard, -(privateAlpha+1), -privateAlpha, depth - 1, end_time);
+          if (score >= (privateAlpha+1)) {
+            debug::SearchDebug("["+std::to_string(privateAlpha)+","+std::to_string(beta)+"] pt"+std::to_string(omp_get_thread_num()), depth);
+            score = -AlphaBeta(privateBoard, -beta, -privateAlpha, depth - 1, end_time);
+          }
+        }
+      }
+      debug::SearchDebug("<<"+parse::MoveToString(move)+" "+std::to_string(score)+" pt"+std::to_string(omp_get_thread_num()), depth);
+      privateBoard.UnMake();
+      if (score >= beta && !finished(end_time)) {
+        private_best_local_move = move;
+        table::SaveEntry(privateBoard.get_hash(), move, score);
+        go = false;//set go to false to skip rest of the moves in this branch
+        //flush the values to make them visible to all
+#pragma omp flush(go)
+      }
+      if (score > privateAlpha) {
+        privateAlpha = score;
+        if (settings::alphaPropagation && ((privateAlpha - settings::alphaChange) > alpha)) {//Todo what is relevant improvement to cut more(best so far 15, maybe us proportional improvement)
+#pragma omp atomic write
+          alpha = privateAlpha;
+#pragma omp flush(alpha)
+        }
+        private_best_local_move = move;
+      }
+    }//end local search
+#pragma omp critical
+    {
+      if(privateAlpha>alpha){
+        best_move = private_best_local_move;
+        alpha=privateAlpha;
+      }
+    }// end omp critical
+  }//end omp parallel
+  if(finished(end_time)){
+    return 0;
+  }
+  if(!go) {
+    return beta;
+  }
+  return alpha;
+}
+
 inline bool finished(Time end_time){
   return end_time < now();
 }
